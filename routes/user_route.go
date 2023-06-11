6b3a3239e20d9c100a2372272b48ca4d593033bb
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2023-06-11 17:42:44 +0300

    feat: add profile controller in profile routes

--- routes/user_route.go
+++ routes/user_route.go
@@ -5,6 +5,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// ProfileRoutes sets up the profile routes for the User.
+func ProfileRoutes(userRoutes *gin.RouterGroup) {
+	userRoutes.GET("/profile", user.GetProfileController)
+	userRoutes.POST("/profile/update", user.UpdateProfileController)
+}
+
 // AddressRoutes sets up the address routes of the user.
 func AddressRoutes(addressRoutes *gin.RouterGroup) {
 	addressRoutes.GET("/address", user.GetAddressController)
