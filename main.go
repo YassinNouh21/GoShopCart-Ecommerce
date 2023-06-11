91422a4f2de012c5a88385b17bb07c42eeab66d5
Yassin Nouh <70436855+YassinNouh21@users.noreply.github.com>
2023-06-11 12:10:24 +0300

    feat: Initialize main entry point

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		// Handle error loading .env file
+		return
+	}
+
+	// Get the port from the environment variable
+	port := os.Getenv("PORT")
+
+	// Create a new Gin router with default middleware
+	router := gin.Default()
+	router.Use(gin.Logger())
+
+	// Initialize database client and collections
+
+	// Set up authentication routes under /auth
+
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Route not defined",
+		})
+	})
+
+	// Run the server on the specified port
+	router.Run(port)
+}
